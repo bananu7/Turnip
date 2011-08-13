7e29484711201329d99305ca7a1ba8a09da67991
Christo Mitov <admin@christomitov.com>
2011-08-12 22:03:54 -0400

    updated env.hs

--- Env.hs
+++ Env.hs
@@ -6,8 +6,6 @@ import LuaAS
 -- 
 --data Env = Env { syms : [(Sym, Allocation)], insts : [String], funcs : [(String, Signature)] }
 
-
-
 --foldl compile initialEnv stmts
 
 --compile (Env syms insts) (Do blk) = let (Env _ i) = foldl compile (Env syms []) (statements blk)  in  Env 

85dcf1122b10d36d60f42cbad949ec4627638bf2
bananu7 <bananu7@o2.pl>
2017-03-19 22:31:58 +0100

    Added even more table tests

--- Test/TestEval.hs
+++ Test/TestEval.hs
@@ -98,9 +98,17 @@ spec = do
         describe "tables" $ do
             it "should handle table assignments" $ do
                 runParse "t = {}; t[1] = 4; return t[1]" `shouldBe` [Number 4.0]
-            it "should handle consecutive arrays" $ do
+            it "should handle consecutive array constructors" $ do
                 runParse "t = { 4, 5, 6 }; return t[0], t[1], t[2], t[3]" 
                     `shouldBe` [Nil, Number 4.0, Number 5.0, Number 6.0]
+            it "should handle mixed array constructors" $ do
+                runParse "t = { 1, x = 2 }; return t[1], t[\"x\"]"
+                    `shouldBe` [Number 1.0, Number 2.0]
+                runParse "t = { 1, x = 2, 3 }; return t[1], t[\"x\"], t[2]"
+                    `shouldBe` [Number 1.0, Number 2.0, Number 3.0]
+            it "should handle table access syntax sugar" $ do
+                runParse "t = { x = 1 }; return t.x" `shouldBe` [Number 1.0]
+                runParse "t = {}; t.x = 2; return t[\"x\"]" `shouldBe` [Number 2.0]
 
         describe "while loop" $ do
             it "should properly skip a loop with a false clause" $ do

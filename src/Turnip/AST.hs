f1325dd3596a82f382737f5d84109116ae52af15
bananu7 <bananu7@o2.pl>
2018-02-20 22:17:45 +0100

    Changed the Show instances of AST to another class

--- src/Turnip/AST.hs
+++ src/Turnip/AST.hs
@@ -47,7 +47,6 @@ data Expr = Number Double
           | Lambda [Name] HasEllipsis Block
           deriving (Show, Eq)
 
-
 data Stmt = Do Block
           | While Expr Block
           | Until Expr Block
@@ -59,50 +58,11 @@ data Stmt = Do Block
           | LocalDecl [Name]
           | CallStmt Expr [Expr]
           | MemberCallStmt Expr Name [Expr]
-          deriving Eq
-
-instance Show Stmt where
-    show (Do x) = show x
-    show (Return xs) = "return " ++ show xs
-    show (Assignment lvals exprs) = intercalate "\n" $ zipWith (\lval expr -> 
-        (show lval) ++ " := " ++ (show expr)) lvals exprs
-    show (CallStmt f params) = show f ++ "(" ++ show params ++ ")"
-    show (MemberCallStmt obj f params) = show obj ++ ":" ++ f ++ "(" ++ show params ++ ")"
-    show (If (block:elseIfBlocks) elseBlock) = "if " ++ show block ++ elseIfBlocksStr ++ elseBlockStr
-      where
-        elseIfBlocksStr = concatMap (("\nelseif " ++) . show) elseIfBlocks
-        elseBlockStr = maybe "" (("\nelse " ++) . show) elseBlock
-    show (LocalDecl names) = "local " ++ (intercalate "," names)
-
-    show (For names (ForNum a b ms) block) = concat [
-        "for ",
-        (intercalate "," names),
-        " = ",
-        show a,
-        ",",
-        show b,
-        stepStr,
-        " do",
-        show block
-        ," end"
-        ]
-        where
-          stepStr = case ms of
-              Nothing -> ""
-              Just s -> "," ++ show s
-
-    show (For names (ForIter es) block) =
-        "for " ++ (intercalate "," names) ++ " in " ++ (intercalate "," (map show es)) ++ " do" ++ show block ++ "end"
-
-    show Break = "break"
+          deriving (Show,Eq)
 
 data ForGen = ForNum Expr Expr (Maybe Expr) -- for var = min,max,step do
             | ForIter [Expr]                -- for x in y do
             deriving (Show, Eq)
 
 data Block = Block [Stmt]
-    deriving (Eq)
-
-instance Show Block where
-    show (Block stmts) = "\n" ++ (intercalate "\n" . map show $ stmts)
-
+    deriving (Show, Eq)
